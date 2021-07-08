270965ff71bb8afc07ad6947ea4cfa797991d97e
Christian Bartley <christianbart13y@gmail.com>
2021-07-08 10:00:07 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const program = require('commander');
+const chalk = require('chalk');
+const clipboardy = require('clipboardy');
+
+const createPassword = require('./utils/createPassword');
+const savePassword = require('./utils/savePassword');
+
+const log = console.log;
+
+program.version('1.0.0').description('Simple Password Generator');
+
+program
+  .option('-l, --length <number>', 'length of password', '8')
+  .option('-s, --save', 'save password to passwords.txt')
+  .option('-nn --no-numbers', 'remove numbers')
+  .option('-ns --no-symbols', 'remove symbols')
+  .parse();
+
+const { length, save, numbers, symbols } = program.opts();
+
+const generatedPassword = createPassword(length, numbers, symbols);
+
+if (save) {
+  savePassword(generatedPassword);
+}
+
+clipboardy.writeSync(generatedPassword);
+
+log(chalk.blue('Generated Password: ') + chalk.bold(generatedPassword));
+log(chalk.yellow('Password copied to clipboard'));
